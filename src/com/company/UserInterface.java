3266e1403655e23f3cd6ee177a0d628cf2762b39
Simon Pellijeff <simonpellijeff@gmail.com>
2018-04-09 15:39:02 +0200

    Make so user can specify operation

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -6,14 +6,20 @@ public class UserInterface {
     private void evaluateOperation(String operation, double x, double y) {
 
     }
+
+    private double scanDouble(Scanner scan){
+        double output = scan.nextDouble();
+        scan.nextLine();
+        return output;
+    }
     private void getInput(Calculator calc) {
         double x, y;
         String operation = "";
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter first number");
-        x = scan.nextDouble();
+        x = scanDouble(scan);
         System.out.println("Please enter second number");
-        y = scan.nextDouble();
+        y = scanDouble(scan);
         System.out.println("What do you want to do?");
         operation = scan.nextLine();
         evaluateOperation(operation, x, y);
