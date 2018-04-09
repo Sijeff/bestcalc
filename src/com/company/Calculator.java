25f8e0ee8c701ded9cea6385ddad014a4a9265db
Maria Sjölin <mia.sjolin@gmail.com>
2018-04-09 14:55:42 +0200

    adding subtraction-method

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -6,8 +6,19 @@ public class Calculator {
         return x + y;
     }
     //methods go here!
+
     public double multiply(double x, double y) {
         return x * y;
+    }
+
+
+
+
+
+
+
+    public int subtraction(int x, int y){
+        return x-y;
 
     }
 }
